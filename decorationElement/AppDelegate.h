f4a38830262310bd54a44478b1922cb3a20f6dd3
Adrian Ortuzar <adrian.ortuzar@scaura.com>
2015-09-11 09:30:54 +0200

    Initial Commit

---
+++ decorationElement/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  decorationElement
+//
+//  Created by Adrian Ortuzar on 11/09/15.
+//  Copyright (c) 2015 Adrian Ortuzar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ decorationElement/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  decorationElement
+//
+//  Created by Adrian Ortuzar on 11/09/15.
+//  Copyright (c) 2015 Adrian Ortuzar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
