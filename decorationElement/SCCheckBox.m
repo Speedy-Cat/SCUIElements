428327e4a4d30194a410eba6311a5a342bba17f2
adrian_ortuzar <adrianortuzar@gmail.com>
2015-09-13 19:21:28 +0200

    check box button

--- decorationElement/SCCheckBox.m
+++ decorationElement/SCCheckBox.m
@@ -8,6 +8,14 @@
 
 #import "SCCheckBox.h"
 
+@interface SCCheckBox ()
+
+
+@property (nonatomic) BOOL isChecked;
+
+
+@end
+
 @implementation SCCheckBox
 
 -(instancetype)initWithPosition:(CGPoint)position
@@ -15,24 +23,62 @@
     self = [super initWithFrame:CGRectMake(50, 200, 200, 26)];
     if (self) {
         
+    
+        // images for diferent states size
+        CGRect rect = CGRectMake(0, 0, 16, 16);
         
-        // checkbox image
-        UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(5, 26/2 - 16/2, 16, 16)];
-        imageView.backgroundColor = [UIColor orangeColor];
-        [self addSubview:imageView];
+        //unCheckImg
+        UIGraphicsBeginImageContext(rect.size);
+                CGContextRef context = UIGraphicsGetCurrentContext();
+        CGContextSetFillColorWithColor(context, [[UIColor orangeColor] CGColor]);
+        CGContextFillRect(context, rect);
+        UIImage *unCheckImg = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+
+        // checkImg
+        UIGraphicsBeginImageContext(rect.size);
+        CGContextSetFillColorWithColor(context, [[UIColor yellowColor] CGColor]);
+        CGContextFillRect(context, rect);
+        UIImage *checkImg = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        
+        //hileghted
+        UIGraphicsBeginImageContext(rect.size);
+        CGContextSetFillColorWithColor(context, [[UIColor purpleColor] CGColor]);
+        CGContextFillRect(context, rect);
+        UIImage *hilighted = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        
+        // set images for diferent states
+        [self setImage:unCheckImg forState:UIControlStateNormal];
+        [self setImage:hilighted forState:UIControlStateHighlighted];
+        [self setImage:checkImg forState:UIControlStateSelected];
         
         // title
-                [self setTitle:@"Female" forState:UIControlStateNormal];
+        [self setTitle:@"Female" forState:UIControlStateNormal];
         self.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
-        self.titleLabel.font = [UIFont systemFontOfSize:14];
-        UIEdgeInsets edge = UIEdgeInsetsMake(0, 26, 0, 0);
-        [self setTitleEdgeInsets:edge];
-        
 
         
+        // touch event
+        [self addTarget:self action:@selector(buttonClicked:) forControlEvents:UIControlEventTouchUpInside];
+        
+        // backgroudnd color
         self.backgroundColor = [UIColor blackColor];
+        
+        
+        [self buttonClicked:nil];
     }
     return self;
+}
+
+- (IBAction) buttonClicked: (id)sender
+{
+    if (self.selected) {
+        self.selected = NO;
+    }
+    else{
+        self.selected = YES;
+    }
 }
 
 /*
