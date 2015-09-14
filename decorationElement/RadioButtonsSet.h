9dfaf1d22d8c637017390169973359982d148b34
adrian_ortuzar <adrianortuzar@gmail.com>
2015-09-14 08:27:36 +0200

    wip

---
+++ decorationElement/RadioButtonsSet.h
@@ -0,0 +1,13 @@
+//
+//  RadioButtonsSet.h
+//  decorationElement
+//
+//  Created by Adrian Ortuzar on 13/09/15.
+//  Copyright (c) 2015 Adrian Ortuzar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RadioButtonsSet : NSSet
+
+@end
---
+++ decorationElement/RadioButtonsSet.m
@@ -0,0 +1,13 @@
+//
+//  RadioButtonsSet.m
+//  decorationElement
+//
+//  Created by Adrian Ortuzar on 13/09/15.
+//  Copyright (c) 2015 Adrian Ortuzar. All rights reserved.
+//
+
+#import "RadioButtonsSet.h"
+
+@implementation RadioButtonsSet
+
+@end
